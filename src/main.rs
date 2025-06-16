a71e66c82d948ccb3cba1167783355cfc71fd127
Maxime Colliat <mcolliat21@gmail.com>
2025-06-16 11:05:10 +0200

    add app icon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use egui::IconData;
 use fractals_rs::constant::{HEIGHT, WIDTH};
 use fractals_rs::structs::fractal_app::FractalApp;
 
@@ -6,8 +7,11 @@ use fractals_rs::structs::fractal_app::FractalApp;
 mod benches;
 
 fn main() -> Result<(), eframe::Error> {
+    let icon_data = load_icon();
+
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([WIDTH, HEIGHT]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([WIDTH, HEIGHT])
+        .with_icon(icon_data),
         ..Default::default()
     };
 
@@ -17,3 +21,22 @@ fn main() -> Result<(), eframe::Error> {
         Box::new(|_cc| Ok(Box::<FractalApp>::default())),
     )
 }
+
+fn load_icon() -> IconData {
+    let (icon_rgba, icon_width, icon_height) = {
+        let icon = include_bytes!("../assets/fractale.png");
+        let image = image::load_from_memory(icon)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    IconData {
+        rgba: icon_rgba,
+        width: icon_width,
+        height: icon_height,
+    }
+}
+
